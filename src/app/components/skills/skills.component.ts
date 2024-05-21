c358495ab55892625eaf4971fbab6f71301a61bb
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-05-22 00:04:47 +0100

    skills section

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,6 +1,8 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { Skill } from '../../models/skill.model';
+import { SkillsService } from '../../services/skills/skills.service';
 
 @Component({
   selector: 'app-skills',
@@ -10,5 +12,13 @@ import { LucideAngularModule } from 'lucide-angular';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
+  skills: Skill [];
 
+  constructor(skillsService: SkillsService) {
+    this.skills = skillsService.getSkills();
+  }
+
+  trackBySkill(index: number, skill: any): number {
+    return skill.id;
+  }
 }
---
+++ src/app/models/skill.model.ts
@@ -0,0 +1,11 @@
+export class Skill {
+    id: number;
+    name: string;
+    imagePath: string;
+
+    constructor (id: number, name: string, imagePath: string) {
+        this.id = id;
+        this.name = name;
+        this.imagePath = imagePath;
+    }
+}
