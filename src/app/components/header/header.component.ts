4d93afbbfa2607fd92510c0a7355a58bb99b9c2d
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-05-23 19:53:11 +0100

    side menu & responsive home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,25 @@
-import { ViewportScroller } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgOptimizedImage, ViewportScroller } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @Input() inputFromParent: boolean = false;
+  @Output() updatedInput = new EventEmitter <boolean> ();
+
   constructor(private viewportScroller: ViewportScroller) {}
 
   public onScrollTo(elementId: string) {
     this.viewportScroller.scrollToAnchor(elementId);
+  }
+
+  toggleSideMenu() {
+    this.updatedInput.emit(!this.inputFromParent)
   }
 }
