902779418517d1d071194093b9233fc4dcc0bfeb
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-05-25 21:47:13 +0100

    testing

--- src/app/components/contact/contact.component.spec.ts
+++ src/app/components/contact/contact.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
 import { LUCIDE_ICONS, LucideIconProvider, Send, UserRound } from 'lucide-angular';
+import { By } from '@angular/platform-browser';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -24,5 +25,61 @@ describe('ContactComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have email', () => {
+    const fixture = TestBed.createComponent(ContactComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    expect(app.email).toEqual('ricardoliveira5ro@gmail.com');
+  });
+
+  it('should render title in h3 tag with value and icon', () => {
+    const fixture = TestBed.createComponent(ContactComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    const h3 = compiled.querySelector('h3');
+
+    expect(h3.textContent).toBe('Contact');
+
+    const iconElement = fixture.debugElement.query(By.css('lucide-icon')).nativeElement;
+    
+    expect(iconElement).toBeTruthy();
+    expect(iconElement.getAttribute('name')).toBe('user-round');
+    expect(iconElement.getAttribute('color')).toBe('var(--color-primary)');
+  });
+
+  it('should render contact description in p tag with value', () => {
+    const fixture = TestBed.createComponent(ContactComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    const p = compiled.querySelector('p');
+
+    expect(p.textContent).toBe('I would love to hear from you! Whether you have a question, feedback, or just want to say hello, feel free to get in touch. I will get back to you as soon as possible.');
+  });
+
+  it('should have a correct mailto link', () => {
+    const fixture = TestBed.createComponent(ContactComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    const a = compiled.querySelector('a');
+
+    expect(a.href).toBe('mailto:ricardoliveira5ro@gmail.com');
+    expect(a.target).toBe('_blank');
+    expect(a.rel).toBe('noopener noreferrer');
+  });
+
+  it('should render contact description in p tag with value', () => {
+    const fixture = TestBed.createComponent(ContactComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    const h3 = compiled.querySelector('p');
+
+    expect(h3.textContent).toBe('I would love to hear from you! Whether you have a question, feedback, or just want to say hello, feel free to get in touch. I will get back to you as soon as possible.');
+  });
+
+  it('should render contact info with value and icon', () => {
+    const emailIcon = fixture.debugElement.query(By.css('.icon')).nativeElement;
+    const emailParagraph = fixture.debugElement.query(By.css('.contact-email')).nativeElement;
+    
+    expect(emailIcon).toBeTruthy();
+    expect(emailIcon.getAttribute('name')).toBe('send');
+    expect(emailParagraph.textContent).toBe('ricardoliveira5ro@gmail.com');
   });
 });
--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 import { Github, LUCIDE_ICONS, Linkedin, LucideIconProvider } from 'lucide-angular';
+import { By } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -24,5 +25,33 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have name and year', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    expect(app.name).toEqual('Ricardo Oliveira');
+    expect(app.year).toEqual(2024);
+  });
+
+  it('should render the correct name and year in p and span tags', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    const p = compiled.querySelector('.footer-section > div > p');
+    const span = p.querySelector('span');
+
+    fixture.detectChanges();
+
+    expect(p.textContent).toContain('Ricardo Oliveira');
+    expect(span.textContent).toBe('/ 2024');
+  });
+
+  it('should render the correct icons', () => {
+    const githubIcon = fixture.debugElement.query(By.css('lucide-icon[name="github"]')).nativeElement;
+    const linkedinIcon = fixture.debugElement.query(By.css('lucide-icon[name="linkedin"]')).nativeElement;
+    
+    expect(githubIcon).toBeTruthy();
+    expect(linkedinIcon).toBeTruthy();
   });
 });
