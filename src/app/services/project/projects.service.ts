5aa59dc54715e1ac1b1eeaadcd25c7d171c558d0
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-09-20 10:18:58 +0100

    add PadelHub API project

--- src/app/services/project/projects.service.ts
+++ src/app/services/project/projects.service.ts
@@ -7,32 +7,11 @@ import { Project } from '../../models/project.model';
 export class ProjectsService {
   private projects: Project[] = [
     new Project(
-      "Show Tracker", 
-      "../../assets/projects/showtracker.png", 
-      "Android application designed to help users keep track of the movies they've watched and the episodes of TV shows they're currently viewing", 
-      ["Kotlin", "Android", "Jetpack Compose", "MVVM"],
-      "https://github.com/ricardoliveira5ro/ShowTracker"
-    ),
-    new Project(
-      "Spendaholic", 
-      "../../assets/projects/spendaholic.png", 
-      "Android application built to help users keep track of their expenses. Easily log expenses, categorize spending, and visualize your financial trends with insightful statistics", 
-      ["Kotlin", "Android", "Jetpack Compose", "MVVM"],
-      "https://github.com/ricardoliveira5ro/Currency-Conversion-API"
-    ),
-    new Project(
-      "CurrenSee Tool", 
-      "../../assets/projects/currenSee.png", 
-      "Web app that allows users to easily convert amounts from one currency to another using real-time exchange rates", 
-      ["Next JS", "JavaScript", "Tailwind CSS"],
-      "https://github.com/ricardoliveira5ro/Currency-Conversion-Tool"
-    ),
-    new Project(
-      "CurrenSee API", 
-      "../../assets/projects/currenSee_API.png", 
-      "Currency Conversion API provide real-time currency conversion rates between various currencies", 
-      ["Node JS", "Express JS"],
-      "https://github.com/ricardoliveira5ro/Currency-Conversion-API"
+      "PadelHub API", 
+      "../../assets/projects/padelhubapi.png", 
+      "PadelHub API is a RESTful API designed to manage padel club activities such as user registration, login, and reservations.", 
+      ["Java", "Spring", "Spring Boot", "Hibernate"],
+      "https://github.com/ricardoliveira5ro/PadelHubAPI"
     ),
     new Project(
       "Word Guesser Bot", 
@@ -40,20 +19,6 @@ export class ProjectsService {
       "Discord bot that allows users to play a word guessing game within their Discord server", 
       ["Node JS", "Discord JS", "dotenv", "jest"],
       "https://github.com/ricardoliveira5ro/Word-Guesser-Bot"
-    ),
-    new Project(
-      "Portfolio Website", 
-      "../../assets/projects/portfolio.png", 
-      "An interactive resume, describing my professional experiences, personal projects and information about me", 
-      ["Angular JS", "TypeScript", "Tailwind CSS"],
-      "https://github.com/ricardoliveira5ro/Portfolio2.0"
-    ),
-    new Project(
-      "Translator Extension", 
-      "../../assets/projects/extension.png", 
-      "Browser extension that allows users to translate selected text on any webpage. When a user selects text, a Translate button appears next to the selection, allowing for quick and easy translation.", 
-      ["JavaScript", "HTML", "CSS"],
-      "https://github.com/ricardoliveira5ro/Translate-Extension"
     ),
     new Project(
       "PathwayAPI", 
@@ -69,6 +34,48 @@ export class ProjectsService {
       ["Ruby", "Ruby on Rails", "Postgresql", "GraphQL API"],
       "https://github.com/ricardoliveira5ro/PathwayAPI-GraphQL"
     ),
+    new Project(
+      "Portfolio Website", 
+      "../../assets/projects/portfolio.png", 
+      "An interactive resume, describing my professional experiences, personal projects and information about me", 
+      ["Angular JS", "TypeScript", "Tailwind CSS"],
+      "https://github.com/ricardoliveira5ro/Portfolio2.0"
+    ),
+    new Project(
+      "Spendaholic", 
+      "../../assets/projects/spendaholic.png", 
+      "Android application built to help users keep track of their expenses. Easily log expenses, categorize spending, and visualize your financial trends with insightful statistics", 
+      ["Kotlin", "Android", "Jetpack Compose", "MVVM"],
+      "https://github.com/ricardoliveira5ro/Currency-Conversion-API"
+    ),
+    new Project(
+      "Show Tracker", 
+      "../../assets/projects/showtracker.png", 
+      "Android application designed to help users keep track of the movies they've watched and the episodes of TV shows they're currently viewing", 
+      ["Kotlin", "Android", "Jetpack Compose", "MVVM"],
+      "https://github.com/ricardoliveira5ro/ShowTracker"
+    ),
+    new Project(
+      "Translator Extension", 
+      "../../assets/projects/extension.png", 
+      "Browser extension that allows users to translate selected text on any webpage. When a user selects text, a Translate button appears next to the selection, allowing for quick and easy translation.", 
+      ["JavaScript", "HTML", "CSS"],
+      "https://github.com/ricardoliveira5ro/Translate-Extension"
+    ),
+    new Project(
+      "CurrenSee Tool", 
+      "../../assets/projects/currenSee.png", 
+      "Web app that allows users to easily convert amounts from one currency to another using real-time exchange rates", 
+      ["Next JS", "JavaScript", "Tailwind CSS"],
+      "https://github.com/ricardoliveira5ro/Currency-Conversion-Tool"
+    ),
+    new Project(
+      "CurrenSee API", 
+      "../../assets/projects/currenSee_API.png", 
+      "Currency Conversion API provide real-time currency conversion rates between various currencies", 
+      ["Node JS", "Express JS"],
+      "https://github.com/ricardoliveira5ro/Currency-Conversion-API"
+    )
   ];
 
   constructor() { }
